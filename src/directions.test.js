fc5d6f5c224aaa91d6258e907c4e399caf28455d
zohaib <syedzohaibakhter@gmail.com>
2020-09-25 21:02:58 +0800

    testing

--- src/directions.test.js
+++ src/directions.test.js
@@ -2,20 +2,20 @@ const expect = require('expect');
 const directions = require('./directions.js');
 
 before(() => {
-  process.env.NODE_ENV = 'test';
+    process.env.NODE_ENV = 'test';
 });
 
 it('able to check if a coordinate is valid or invalid', () => {
-  let wrongCoordinate = [2, 5];
-  expect(directions.isValidCoordinate(wrongCoordinate)).toBeFalsy();
+    let wrongCoordinate = [2, 5];
+    expect(directions.isValidCoordinate(wrongCoordinate)).toBeFalsy();
 });
 
 it('other than this commands N, S, E, W the direction will be invalid', () => {
-  let invalidDirectionCode = 'T';
-  expect(directions.isValidDirection(invalidDirectionCode)).toBeFalsy();
+    let invalidDirectionCode = 'T';
+    expect(directions.isValidDirection(invalidDirectionCode)).toBeFalsy();
 });
 
 it('should be getting the correct direction full name (North, South, East, West) given a short code (N, S, E, W', () => {
-  let directionCode = 'N';
-  expect(directions.getDirectionName(directionCode)).toBe('NORTH');
+    let directionCode = 'N';
+    expect(directions.getDirectionName(directionCode)).toBe('NORTH');
 });
