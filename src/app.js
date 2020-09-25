c374d1d374ee7f5af771f11d7d06663d78ab29bd
zohaib <syedzohaibakhter@gmail.com>
2020-09-25 16:47:41 +0800

    commit

---
+++ src/app.js
@@ -0,0 +1,8 @@
+const helper = require("./helper");
+
+const interface = () => {
+    helper.heading();
+    helper.userGuide();
+};
+
+interface();
---
+++ src/helper.js
@@ -0,0 +1,9 @@
+const heading = () => {
+    console.log('Toy Robot Application');
+};
+
+const userGuide = () => {
+    console.log('start typing new command');
+};
+
+module.exports = { heading, userGuide };
